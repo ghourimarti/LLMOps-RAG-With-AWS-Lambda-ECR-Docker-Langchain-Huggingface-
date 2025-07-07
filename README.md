729b75037f526f776581f79c8dba9d25ba39ad57
Zain Ul Abdin <ghourimarti@gmail.com>
2025-07-07 20:35:43 +0500

    add requiremnts.txt...........

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Deploying RAG APPLICATION using AWS(AWS lambda,AWS ECR), Langchain, Huggingface, Docker
 
+
+https://cloud.google.com/architecture/mlops-continuous-delivery-and-automation-pipelines-in-machine-learning
+
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 conda create -p llmops python
 conda activate "D:\Coursera Courses (NVME)\LLM freecodecamp\LLMOps-sunnysavita\LLMOps-RAG-With-AWS-Lambda-ECR-Docker-Langchain-Huggingface-\llmops"
 
+
+https://cloud.google.com/architecture/mlops-continuous-delivery-and-automation-pipelines-in-machine-learning
